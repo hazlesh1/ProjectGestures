164f12d57a040d5e4e59ef96826c89e9fee2e707
Haz <115780173+hazlesh1@users.noreply.github.com>
2026-07-16 09:50:09 +0200

    implement VL53L0X I2C driver and integrate sensor initialization into main (debugged)

--- drivers/vl53l0x_driver.h
+++ drivers/vl53l0x_driver.h
@@ -1,36 +1,45 @@
 #ifndef VL53L0X_DRIVER_H
 #define VL53L0X_DRIVER_H
 
-#include "hardware/i2c.h"
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include "hardware/i2c.h"
 
-// Hardware config (change this based on your pins for people modifying the
-// code)
+
+// Hardware Config, change this if modifying based on hardware
 #define I2C_PORT i2c0
 #define PIN_SDA 4
 #define PIN_SCL 5
-#define XSHUT_LEFT 6
-#define XSHUT_RIGHT 7
 
-// I2C address map
+// Default address for sensors
 #define VL53L0X_DEFAULT_ADDR 0x29
-#define SENSOR_LEFT_ADDR 0x30
-#define SENSOR_RIGHT_ADDR 0x31
 
-// ---- Sentinel value returned when a reading isn't ready yet ----
+// Sentinel value when sensor not ready for reading
 #define VL53L0X_NOT_READY 0xFFFF
+
+// Number of sensors
+#define SENSOR_COUNT 2
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
+typedef struct {
+    uint8_t xshut_pin;
+    uint8_t i2c_addr;
+    uint8_t stop_variable;
+} vl53l0x_sensor_t;
+
 void vl53l0x_init_bus(void);
-bool vl53l0x_init_sensor_array(void);
-uint16_t vl53l0x_read_distance(uint8_t addr);
+bool vl53l0x_init_sensor(vl53l0x_sensor_t *sensor);
+bool vl53l0x_init_sensors(vl53l0x_sensor_t *sensors, uint8_t count);
+uint16_t vl53l0x_read_distance(vl53l0x_sensor_t *sensor);
 
 #ifdef __cplusplus
 }
 #endif
+
 
 #endif // VL53L0X_DRIVER_H
--- main.cpp
+++ main.cpp
@@ -1,102 +1,129 @@
-#include "hardware/gpio.h"
-#include "hardware/i2c.h"
-#include "pico/stdio.h"
+// #include "hardware/gpio.h"
+// #include "hardware/i2c.h"
+// #include "pico/stdio.h"
+// #include "pico/time.h"
+// #include <stdio.h>
+// #include "drivers/vl53l0x_driver.h"
+
+// #define I2C_PORT i2c0
+// #define I2C_SDA 4
+// #define I2C_SCL 5
+// #define XSHUT_LEFT 6
+// #define XSHUT_RIGHT 7
+// #define SENSOR_LEFT_ADDR 0x30
+// #define SENSOR_RIGHT_ADDR 0x29
+
+// void vl53l0x_set_address(uint8_t old_addr, uint8_t new_addr) {
+//   uint8_t data[2];
+
+//   data[0] = 0x8A;
+//   data[1] = new_addr;
+
+//   i2c_write_blocking(I2C_PORT, old_addr, data, 2, false);
+// }
+
+// void setup_sensors() {
+//   gpio_init(XSHUT_LEFT);
+//   gpio_set_dir(XSHUT_LEFT, GPIO_OUT);
+
+//   gpio_init(XSHUT_RIGHT);
+//   gpio_set_dir(XSHUT_RIGHT, GPIO_OUT);
+
+//   // offfff
+//   gpio_put(XSHUT_LEFT, 0);
+//   gpio_put(XSHUT_RIGHT, 0);
+
+//   sleep_ms(10);
+
+//   // on left
+//   gpio_put(XSHUT_LEFT, 1);
+
+//   sleep_ms(50);
+
+//   // change sensor address
+//   vl53l0x_set_address(0x29, SENSOR_LEFT_ADDR);
+
+//   // on right
+//   gpio_put(XSHUT_RIGHT, 1);
+
+//   sleep_ms(50);
+// }
+
+// // int main() {
+// //   stdio_init_all();
+
+// //   // I2C Initialisation. Using it at 400Khz.
+// //   i2c_init(I2C_PORT, 400 * 1000);
+
+// //   gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+// //   gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+// //   gpio_pull_up(I2C_SDA);
+// //   gpio_pull_up(I2C_SCL);
+
+// //   // gpio_init(XSHUT_LEFT);
+// //   // gpio_set_dir(XSHUT_LEFT, GPIO_OUT);
+
+// //   // gpio_init(XSHUT_RIGHT);
+// //   // gpio_set_dir(XSHUT_RIGHT, GPIO_OUT);
+
+// //   // gpio_put(XSHUT_LEFT, 1);
+// //   // gpio_put(XSHUT_RIGHT, 1);
+
+// //   // For more examples of I2C use see
+// //   // https://github.com/raspberrypi/pico-examples/tree/master/i2c
+// //   setup_sensors();
+
+// //   sleep_ms(2000);
+
+// //   // vl53l0x_init(SENSOR_LEFT_ADDR);
+// //   // vl53l0x_init(SENSOR_RIGHT_ADDR);
+
+// //   while (true) {
+// //     printf("Scanning...\n");
+
+// //     for (uint8_t addr = 0x08; addr < 0x77; addr++) {
+
+// //       uint8_t data;
+
+// //       int result = i2c_read_blocking(I2C_PORT, addr, &data, 1, false);
+
+// //       if (result >= 0) {
+// //         printf("Found device at 0x%02X\n", addr);
+// //       }
+// //     }
+
+// //     printf("----------------\n");
+// //     sleep_ms(3000);
+// //   }
+// // }
+
+
+#include "pico/stdlib.h"
 #include "pico/time.h"
 #include <stdio.h>
-
-// I2C defines
-// Pins can be changed, see the GPIO function select table in the datasheet
-for
-// information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 4
-#define I2C_SCL 5
-#define XSHUT_LEFT 6
-#define XSHUT_RIGHT 7
-#define SENSOR_LEFT_ADDR 0x30
-#define SENSOR_RIGHT_ADDR 0x29
-
-void vl53l0x_set_address(uint8_t old_addr, uint8_t new_addr) {
-  uint8_t data[2];
-
-  data[0] = 0x8A;
-  data[1] = new_addr;
-
-  i2c_write_blocking(I2C_PORT, old_addr, data, 2, false);
-}
-
-void setup_sensors() {
-  gpio_init(XSHUT_LEFT);
-  gpio_set_dir(XSHUT_LEFT, GPIO_OUT);
-
-  gpio_init(XSHUT_RIGHT);
-  gpio_set_dir(XSHUT_RIGHT, GPIO_OUT);
-
-  // offfff
-  gpio_put(XSHUT_LEFT, 0);
-  gpio_put(XSHUT_RIGHT, 0);
-
-  sleep_ms(10);
-
-  // on left
-  gpio_put(XSHUT_LEFT, 1);
-
-  sleep_ms(50);
-
-  // change sensor address
-  vl53l0x_set_address(0x29, SENSOR_LEFT_ADDR);
-
-  // on right
-  gpio_put(XSHUT_RIGHT, 1);
-
-  sleep_ms(50);
-}
+#include "drivers/vl53l0x_driver.h"
 
 int main() {
-  stdio_init_all();
+    stdio_init_all();
+    sleep_ms(2000); 
 
-  // I2C Initialisation. Using it at 400Khz.
-  i2c_init(I2C_PORT, 400 * 1000);
+    vl53l0x_init_bus();
 
-  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-  gpio_pull_up(I2C_SDA);
-  gpio_pull_up(I2C_SCL);
+    vl53l0x_sensor_t sensors[2] = {
+        {6, 0x30, 0}, 
+        {7, 0x31, 0},  
+    };
 
-  // gpio_init(XSHUT_LEFT);
-  // gpio_set_dir(XSHUT_LEFT, GPIO_OUT);
+    printf("Initializing sensors...\n");
+    bool ok = vl53l0x_init_sensors(sensors, 2);
+    printf("Sensor init: %s\n", ok ? "OK" : "FAILED");
 
-  // gpio_init(XSHUT_RIGHT);
-  // gpio_set_dir(XSHUT_RIGHT, GPIO_OUT);
+    while (true) {
+        uint16_t left = vl53l0x_read_distance(&sensors[0]);
+        uint16_t right = vl53l0x_read_distance(&sensors[1]);
 
-  // gpio_put(XSHUT_LEFT, 1);
-  // gpio_put(XSHUT_RIGHT, 1);
+        printf("Left: %u mm | Right: %u mm\n", left, right);
 
-  // For more examples of I2C use see
-  // https://github.com/raspberrypi/pico-examples/tree/master/i2c
-  setup_sensors();
-
-  sleep_ms(2000);
-
-  // vl53l0x_init(SENSOR_LEFT_ADDR);
-  // vl53l0x_init(SENSOR_RIGHT_ADDR);
-
-  while (true) {
-    printf("Scanning...\n");
-
-    for (uint8_t addr = 0x08; addr < 0x77; addr++) {
-
-      uint8_t data;
-
-      int result = i2c_read_blocking(I2C_PORT, addr, &data, 1, false);
-
-      if (result >= 0) {
-        printf("Found device at 0x%02X\n", addr);
-      }
+        sleep_ms(100);
     }
-
-    printf("----------------\n");
-    sleep_ms(3000);
-  }
 }
-
